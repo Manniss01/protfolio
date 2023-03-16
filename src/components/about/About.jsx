9933029c963e239b5797d6366730f0fa28211d97
Manniss01 <msnish.p001@gmail.com>
2023-03-16 14:20:28 +0545

    change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Smith-Cv.pdf";
+import CV from "../../assets/manniss.pdf";
 import Info from './Info'
 
 
